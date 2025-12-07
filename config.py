a82a2b49020f41d617e7c01b8c34b5bc97c1878e
Nazik-Bolotbekova <bolotbekova09@gmail.com>
2025-12-07 13:11:35 +0600

    a

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 import os
-
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
 FILTER_GROUP_ID = os.getenv("FILTER_GROUP_ID")
-REQUEST_TOPIC_ID = int(os.getenv("REQUEST_TOPIC_ID"))
-PROBLEM_TOPIC_ID = int(os.getenv("PROBLEM_TOPIC_ID"))
 OPEN_ROUTER_API = os.getenv("OPEN_ROUTER_API")
+
+
+REQUEST_TOPIC_ID = int(os.getenv("REQUEST_TOPIC_ID") or "0")
+PROBLEM_TOPIC_ID = int(os.getenv("PROBLEM_TOPIC_ID") or "0")
+
+
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN не установлен!")
+if not OPEN_ROUTER_API:
+    raise ValueError("❌ OPEN_ROUTER_API не установлен!")
+
+print("Конфиг загружен")
